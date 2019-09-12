e3124791d7c9e0b61350746765230ffde12c5f8e
natalialexandra <natalia.alexandra195@gmail.com>
2019-09-12 14:17:36 +0200

    add update function to change the value at given index

--- my-solutions/closure-api.js
+++ my-solutions/closure-api.js
@@ -21,18 +21,26 @@ let init = () => {
   let db = [];
   let api = [];
 
-  let create = elem => {
+  const create = elem => {
     db.push(elem);
     return db.length;
   };
 
-  let read = () => {
+  const read = () => {
     let dbCopy = [...db];
     return dbCopy;
     // return db.push(dbCopy); // so you can push value from read
   };
 
-  api.push(create, read);
+  const update = (index, elem) => {
+    if (db[index]) {
+      return (db[index] = elem);
+    } else {
+      return "Error, there is no existing element at the given index!";
+    }
+  };
+
+  api.push(create, read, update);
   return api;
 };
 
@@ -43,4 +51,7 @@ console.log(createInDb[0](2));
 console.log(createInDb[0]("sun"));
 console.log(createInDb[1]("new")); // from the outside(api) you can not add new element
 console.log(createInDb[0]("other one"));
+console.log(createInDb[1]()); // access to read
+console.log(createInDb[2](0, "new elem"));
+console.log(createInDb[2](10, "new elem"));
 console.log(createInDb[1]()); // access to read
