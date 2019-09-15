50f449cd97dc8ee1b96584abb196823cf5af0e9e
natalialexandra <natalia.alexandra195@gmail.com>
2019-09-15 12:03:33 +0200

    add comments

--- my-solutions/closure-api.js
+++ my-solutions/closure-api.js
@@ -4,43 +4,72 @@ let init = () => {
   let db = [];
   let api = [];
 
+  // create
   const create = elem => {
     db.push(elem);
-    return db.length;
+    return `New element added! The new length of the array is ${db.length}`;
   };
 
+  // read
   const read = () => {
     let dbCopy = [...db];
     return dbCopy;
-    // return db.push(dbCopy); // so you can push value from read
   };
 
+  // update
   const update = (index, elem) => {
     if (db[index]) {
       return (db[index] = elem);
     } else {
-      //   return "Error, there is no existing element at the given index!";
-      return (
-        create(elem) +
-        ` New element added! The new length of the array is ${db.length}.`
-      );
+      // return "Error, there is no existing element at the given index!";
+      return create(elem);
     }
   };
 
-  api.push(create, read, update);
+  // delete
+  const remove = index => {
+    if (db[index]) {
+      return db.splice(index, 1);
+    } else {
+      return "Error, there is no existing element at the given index!";
+    }
+  };
+
+  api.push(create, read, update, remove);
   return api;
 };
 
 let createInDb = init();
 
+// add new element to array
+console.log("\n-----add new element to array-----------------------");
 console.log(createInDb[0]("first"));
 console.log(createInDb[0](2));
-console.log(createInDb[0]("sun"));
-console.log(createInDb[1]("new")); // from the outside(api) you can not add new element
 console.log(createInDb[0]("other one"));
-console.log(createInDb[1]()); // access to read
+
+// read data from array
+console.log("\n-----print / read array after add-------------------");
+console.log(createInDb[1]());
+
+console.log("\n-----update array-----------------------------------");
 console.log(createInDb[2](0, "new elem"));
-console.log(createInDb[2](10, 100));
 console.log(createInDb[2](20, "7"));
-console.log(createInDb[2](15, "sunshine"));
-console.log(createInDb[1]()); // access to read
+
+// read data after update
+console.log("\n-----print array after update------------------------");
+console.log(createInDb[1]());
+
+// delete element from array
+console.log("\n-----delete element----------------------------------\n");
+console.log("delete element on no existing index");
+console.log(createInDb[3](-1));
+console.log("\ndelete element on existing index");
+console.log(createInDb[3](2));
+
+// read data after delete element
+console.log("\n-----print array after delete------------------------");
+console.log(createInDb[1]());
+
+// check hacking attempt
+// console.log("\nhacking test");
+// console.log(createInDb[1]("new")); // from the outside(api) you can not add new element
