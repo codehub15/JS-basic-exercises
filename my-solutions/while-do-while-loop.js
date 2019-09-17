888a822cc23b8a75d4ff4470281adfa1c921e137
natalialexandra <natalia.alexandra195@gmail.com>
2019-09-17 12:08:05 +0200

    done do-while-loop 1 - 3

--- my-solutions/while-do-while-loop.js
+++ my-solutions/while-do-while-loop.js
@@ -1,4 +1,5 @@
 // while loop
+console.log("#\n while loop");
 
 console.log("\n# 1 --------------------------------------");
 // 1 run along
@@ -63,3 +64,51 @@ for (let i = 0; i < numbers.length; i++) {
     numbers[i] = numbers[i] + 1;
   }
 } */
+
+console.log("\n# do while loop \n");
+// do while loop
+
+console.log("# 1 --------------------------------------");
+// 1 sumOfNumbers
+// Create a program that adds up the numbers in an array (of at least 3 numbers).
+let numbs = [1, 2, 3, 4, 5];
+let numbIndex = 0;
+let sumResult = 0;
+console.log("array with numbers:", numbs);
+do {
+  sumResult += numbs[numbIndex];
+  // console.log(sumResult);
+  ++numbIndex;
+} while (numbIndex < numbs.length);
+console.log(`sum of numbers is ${sumResult}`);
+
+console.log("# 2 --------------------------------------");
+// 2 Add it up
+// Create a program that adds numbers from 1-20 together using a while loop.
+let addItUp = 0;
+let addIndex = 1;
+do {
+  // addItUp += addItUp;
+  console.log(`inside loop add it up is ${(addItUp += addIndex)}`);
+  console.log(`add it up is ${addIndex}`);
+  ++addIndex;
+} while (addIndex <= 20);
+console.log(`outside loop add it up is ${(addItUp += addIndex)}`);
+
+console.log("\n# 3 --------------------------------------");
+// 3 Capitalize
+// Create a program that capitalizes the first letter of each element in an array of names, using a do while loop.
+// Examples: ["matt", "sara", "lara"] ➞ ["Matt", "Sara", "Lara"]
+let names = ["matt", "sara", "lara"];
+let namesIndex = 0;
+let capitalized = [];
+console.log(`array with names before: ${names}`);
+do {
+  let capitalize =
+    names[namesIndex][0].toUpperCase() + names[namesIndex].substring(1);
+
+  capitalized.push(capitalize);
+
+  ++namesIndex;
+} while (namesIndex < names.length);
+console.log(`array with names after: ${capitalized}`);
