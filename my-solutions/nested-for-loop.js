f438a40b8b79ccd6bb98785098afd732bf60b373
natalialexandra <natalia.alexandra195@gmail.com>
2019-09-23 10:47:11 +0200

    add pyramid as a function with nested for-loop

--- my-solutions/nested-for-loop.js
+++ my-solutions/nested-for-loop.js
@@ -112,3 +112,23 @@ for (let i = 0; i < 5; i++) {
   }
   console.log(starTree2);
 }
+
+console.log();
+
+// another way with a function
+const pyramid = n => {
+  const mid = Math.floor((2 * n - 1) / 2);
+  for (let row = 0; row < n; ++row) {
+    //for each row, make empty steps
+    let level = "";
+    for (let col = 0; col < 2 * n - 1; col++) {
+      if (mid - row <= col && mid + row >= col) {
+        level += "*";
+      } else {
+        level += " ";
+      }
+    }
+    console.log(level);
+  }
+};
+pyramid(8);
